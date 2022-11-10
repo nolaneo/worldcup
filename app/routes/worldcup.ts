5e050c63bbaf4ab3aa29fe3abd003247d4e726e8
Conor Mongey <conor@mongey.net>
2022-11-10 21:14:46 +0000

    Use json loading for everything

--- app/routes/worldcup.ts
+++ app/routes/worldcup.ts
@@ -3,31 +3,15 @@ import { inject as service } from '@ember/service';
 import Api from 'worldcup/services/api';
 import { taskFor } from 'ember-concurrency-ts';
 import Sweepstakes from 'worldcup/services/sweepstakes';
-import { data as collegeData } from 'worldcup/data/college';
-import { data as pintmenData } from 'worldcup/data/pintmen';
-import { data as babb } from 'worldcup/data/babb';
-import { data as ashData } from 'worldcup/data/ashbourne';
-
-var fixedData = {
-  college: collegeData,
-  pintmen: pintmenData,
-  babb: babb,
-  ashbourne: ashData,
-};
 
 export default class WorldCup extends Route {
   @service declare api: Api;
   @service declare sweepstakes: Sweepstakes;
 
   async model(params: { id: string }) {
-    //@ts-ignore
-    var players = fixedData[params.id] || null;
-    if (players === null) {
-      // attempt to load from the file system
-      let response = await fetch(`/players/${params.id}.json`);
-      let loadedData = await response.json();
-      players = loadedData.data;
-    }
+    let response = await fetch(`/players/${params.id}.json`);
+    let loadedData = await response.json();
+    let players = loadedData.players;
 
     this.sweepstakes.setPlayers(players);
     await taskFor(this.api.loadModel).perform();
