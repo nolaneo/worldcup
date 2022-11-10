6e762c4ee27f665fe7dcbc4627674c0d5cddd768
Eoin Nolan <eoinnolan@Eoins-MacBook-Pro.local>
2022-11-10 22:49:20 +0000

    just fingerprint css and js

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,7 +5,7 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function (defaults) {
   let app = new EmberApp(defaults, {
     fingerprint: {
-      exclude: ['assets/'],
+      extensions: ['js', 'css'],
     },
     postcssOptions: {
       compile: {
