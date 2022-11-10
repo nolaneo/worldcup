fca668a039cc3f7aa4aa29a6fd4080865ee5f190
Conor Mongey <conor@mongey.net>
2022-11-10 20:27:20 +0000

    Allow loading of data from json files

--- app/routes/worldcup.ts
+++ app/routes/worldcup.ts
@@ -2,31 +2,34 @@ import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 import Api from 'worldcup/services/api';
 import { taskFor } from 'ember-concurrency-ts';
-import Sweepstakes, { Player } from 'worldcup/services/sweepstakes';
+import Sweepstakes from 'worldcup/services/sweepstakes';
 import { data as collegeData } from 'worldcup/data/college';
 import { data as pintmenData } from 'worldcup/data/pintmen';
 import { data as babb } from 'worldcup/data/babb';
 import { data as ashData } from 'worldcup/data/ashbourne';
 
-type SweepstakesId = 'college' | 'pintmen' | 'babb' | 'ashbourne';
+var fixedData = {
+  college: collegeData,
+  pintmen: pintmenData,
+  babb: babb,
+  ashbourne: ashData,
+};
+
 export default class WorldCup extends Route {
   @service declare api: Api;
   @service declare sweepstakes: Sweepstakes;
 
-  async model(params: { id: SweepstakesId }) {
-    if (params.id === 'college') {
-      this.sweepstakes.setPlayers(collegeData as Array<Player>);
-    }
-    if (params.id === 'pintmen') {
-      this.sweepstakes.setPlayers(pintmenData as Array<Player>);
-    }
-    if (params.id === 'babb') {
-      this.sweepstakes.setPlayers(babb as Array<Player>);
-    }
-    if (params.id === 'ashbourne') {
-      this.sweepstakes.setPlayers(ashData as Array<Player>);
+  async model(params: { id: string }) {
+    //@ts-ignore
+    var players = fixedData[params.id] || null;
+    if (players === null) {
+      // attempt to load from the file system
+      let response = await fetch(`/players/${params.id}.json`);
+      let loadedData = await response.json();
+      players = loadedData.data;
     }
 
+    this.sweepstakes.setPlayers(players);
     await taskFor(this.api.loadModel).perform();
     taskFor(this.api.enqueueRefresh).perform();
     return this.api.model;
