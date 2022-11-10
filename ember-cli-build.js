2c536b55dc2b1f5d82e7ceef9dffa88aed266500
Eoin Nolan <eoinnolan@Eoins-MacBook-Pro.local>
2022-11-10 22:45:19 +0000

    disable image fingerprints

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,6 +4,9 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function (defaults) {
   let app = new EmberApp(defaults, {
+    fingerprint: {
+      exclude: ['assets/images/'],
+    },
     postcssOptions: {
       compile: {
         plugins: [
