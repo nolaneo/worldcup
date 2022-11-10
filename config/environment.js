0d68b6c8a0701094cf173a12747eb0c43053385e
Eoin Nolan <eoinnolan@Eoins-MacBook-Pro.local>
2022-11-10 19:17:29 +0000

    fix urk

--- config/environment.js
+++ config/environment.js
@@ -4,7 +4,7 @@ module.exports = function (environment) {
   let ENV = {
     modulePrefix: 'worldcup',
     environment,
-    rootURL: 'worldcup/',
+    rootURL: '/',
     locationType: 'hash',
     EmberENV: {
       FEATURES: {
