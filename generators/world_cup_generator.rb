a94e58811b903ef1ed46bda66fc84f597ecebc94
Eoin Nolan <eoinnolan@Eoins-MacBook-Pro.local>
2022-11-12 15:10:14 +0000

    Balance the selected teams to prevent loads of overlaps

--- generators/world_cup_generator.rb
+++ generators/world_cup_generator.rb
@@ -237,75 +237,149 @@ TEAMS = [
   },
 ]
 
-random = Random.new(seed)
+@random = Random.new(seed)
 
-players.shuffle!(random: random)
+players.shuffle!(random: @random)
 
+number_of_combinations = 10_000
 
-teams_by_tier = TEAMS.group_by { |team| team[:tier] }
+output_teams_by_tier = {}
 
-teams_by_tier.each do |t, teams|
-  raise "#{teams.count} in tier #{t}" unless teams.count == 8
-end
+def generate_player_teams(output:, players:)
 
-output_teams_by_tier = {
-  1 => [],
-  2 => [],
-  3 => [],
-  4 => [],
-}
-
-0.upto(players.count) do |i|
-  puts "Generating output teams #{i}"
-
-  if teams_by_tier.all? { |_, v| v.empty? }
   teams_by_tier = TEAMS.group_by { |team| team[:tier] }
-  end
 
-  1.upto(4) do |tier|
-    team = teams_by_tier[tier].shuffle!(random: random).pop
-    output_teams_by_tier[tier] << team
+
+  players.each_with_index do |player, index|
+
+    if teams_by_tier.values.all? { |teams| teams.empty? }
+      puts "Reset teams"
+      teams_by_tier = TEAMS.group_by { |team| team[:tier] }
+    end
+
+    used_groups_for_player = []
+
+    teams_by_tier.each do |tier, teams|
+      teams.shuffle!(random: @random)
+      available_teams = teams.reject { |team| used_groups_for_player.include?(team[:group]) }
+
+      if available_teams.empty?
+        return false
+      else
+        team = available_teams.pop
+        teams.delete(team)
+        output[tier] << team
+        used_groups_for_player << team[:group]
+      end
+    end
   end
+  
+  puts "Combination found"
+  true
 end
 
+def ensure_unique_groups(output:, players:)
+  players.each_with_index do |player, index|
+    
+    groups = output.keys.map { |tier| output[tier][index][:group] }
+    
+    unless groups.uniq.count == 4
+      return false
+    end
+  end
 
-number_of_combinations = 100_000
+  true
+end
 
-max_overlaps = (players.count / 8.0).floor
+def other_groups_used(output:, player:, swap_tier:)
+  (output.keys - [swap_tier]).map { |tier| output[tier][player][:group] }
+end
+
+def swap(output:, players_with_most_overlaps:, players_with_fewest_overlaps:)
+  player = players_with_most_overlaps.sample(random: @random)
+
+  tier_to_swap = output.keys.sample(random: @random)
+
+  other_groups_used_by_player = other_groups_used(output: output, player: player, swap_tier: tier_to_swap)
+
+  swap_player = players_with_fewest_overlaps.find { |index| !other_groups_used_by_player.include?(output[tier_to_swap][index][:group]) }
+
+  return if swap_player.nil?
+
+  # puts "Swapping teams for player #{player} with player #{swap_player} on tier #{tier_to_swap}"
+
+  a = output[tier_to_swap][player]
+  b = output[tier_to_swap][swap_player]
+
+  output[tier_to_swap][player] = b
+  output[tier_to_swap][swap_player] = a
+end
+
+def balance_overlaps(output:, players:)
+  player_indices = (0..players.count - 1)
+
+  balance_attempts = 0
+
+  while balance_attempts < 100 do
+    players_by_overlap_count = player_indices.group_by do |index|
+      output.keys.select do |tier|
+        team_for_player = output[tier][index]
+
+        output[tier].group_by(&:itself).transform_values(&:count)[team_for_player] > 1
+      end.count
+    end
+
+    keys = players_by_overlap_count.keys.sort
+
+    if keys.count < 2
+      names = players_by_overlap_count.map do |k, v|
+        [k, v.map { |player_index| players[player_index] }]
+      end.to_h
+
+      puts "Final overlap counts: #{names}"
+
+      return true
+    end
+
+    if keys.last - keys.first <= 1
+      names = players_by_overlap_count.map do |k, v|
+        [k, v.map { |player_index| players[player_index] }]
+      end.to_h
+
+      puts "Final overlap counts: #{names}"
+
+      return true
+    end
+
+    balance_attempts += 1
+
+    swap(output: output, players_with_most_overlaps: players_by_overlap_count[keys.last], players_with_fewest_overlaps: players_by_overlap_count[keys.first])
+  end
+end
 
 result = 1.upto(number_of_combinations).find do |i|
   puts "Calculating combination #{i}"
 
-  is_ok = 0.upto(players.count).all? do |i|
-    output_teams_by_tier.keys.map { |tier| output_teams_by_tier[tier][i][:group] }.uniq.count == 4 &&
-    output_teams_by_tier.keys.map { |tier| output_teams_by_tier[tier][i][:name] }.uniq.count >= 4
-  end
+  output = {
+    1 => [],
+    2 => [],
+    3 => [],
+    4 => [],
+  }
 
+  success = generate_player_teams(players: players, output: output)
 
-  is_ok = is_ok && (0).upto(players.count).all? do |player, i|
-    overlaps = 0
-    output_teams_by_tier.keys do |tier|
-      team_for_player = output_teams_by_tier[tier][i][:group]
-      all_instance_of_this_team = output_teams_by_tier[tier].select { |team| team[:group] == team_for_player[:group] }
+  next false unless success
 
-      overlaps += 1 if all_instance_of_this_team.count > 1
-    end
+  next false unless ensure_unique_groups(players: players, output: output)
 
-    next false if overlaps > max_overlaps
-    true
-  end
+  balance_overlaps(players: players, output: output)
 
-  if !is_ok
-    output_teams_by_tier.each { |_, teams| teams.shuffle!(random: random) }
-  end
+  next false unless ensure_unique_groups(players: players, output: output)
 
-  is_ok
-end
+  output_teams_by_tier = output
 
-if result.nil?
-  raise "No combo found"
-else
- puts "Combo found"
+  true
 end
 
 
