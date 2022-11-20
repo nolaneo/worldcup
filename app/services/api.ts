bd0756d0060da6c435cbd5233f62e3ac23aa149f
Eoin Nolan <eoinnolan@Eoins-MacBook-Pro.local>
2022-11-20 16:46:30 +0000

    Fix live scores

--- app/services/api.ts
+++ app/services/api.ts
@@ -6,6 +6,7 @@ import { taskFor } from 'ember-concurrency-ts';
 const standingsEndpoint = `https://standings.uefa.com/v1/standings?groupIds=2007941,2007942,2007943,2007944,2007946,2007945,2007947,2007948`;
 const fixturesEnpoint = `https://match.uefa.com/v5/matches?competitionId=17&utcOffset=1&order=ASC&offset=0&fromDate=2022-11-20&toDate=2022-12-18&limit=100`;
 const apiKey = `ceeee1a5bb209502c6c438abd8f30aef179ce669bb9288f2d1cf2fa276de03f4`;
+const liveScoreEndpoint = `https://api.fifa.com/api/v3/live/football/range?from=2022-11-20T00:00:00Z&to=2023-07-20T00:00:00Z&IdSeason=255711&language=en`;
 
 export type CountryCode =
   | 'ARG'
@@ -86,10 +87,30 @@ export interface TeamStanding {
 export interface GroupStandingWireFormat {
   items: Array<TeamStanding>;
 }
+
+export interface MatchResult {
+  MatchTime: string;
+  GroupName: string;
+  Date: string;
+  HomeTeam: {
+    IdCountry: string;
+    Score: number;
+  };
+  AwayTeam: {
+    IdCountry: string;
+    Score: number;
+  };
+  MatchStatus: number; // 3 == live
+}
+export interface LiveScoreWireFormat {
+  Results: Array<MatchResult>;
+}
+
 export default class Api extends Service {
   @tracked model!: {
     standings: Array<GroupStandingWireFormat>;
     fixtures: Array<FixtureWireformat>;
+    liveScores: Array<MatchResult>;
   };
 
   @dropTask
@@ -97,15 +118,43 @@ export default class Api extends Service {
     let result = yield Promise.all([
       taskFor(this.loadStandings).perform(),
       taskFor(this.loadFixtures).perform(),
+      taskFor(this.liveScores).perform(),
     ]);
     this.model = {
       standings: [],
       fixtures: [],
+      liveScores: [],
     };
     this.model = {
       standings: result[0] as Array<GroupStandingWireFormat>,
       fixtures: result[1] as Array<FixtureWireformat>,
+      liveScores: result[2].Results as Array<MatchResult>,
     };
+
+    let liveFixtures = this.model.liveScores.filter(
+      (fixture) => fixture.MatchStatus === 3
+    );
+
+    // The UEFA API does not seem to be returning live scores for FIFA competitions,
+    // so just inject the scores from the FIFA API here to avoid having to rewrite the app.
+    liveFixtures.forEach((liveFixture) => {
+      let matchingFixture = this.model.fixtures.find(
+        (fixture) =>
+          fixture.kickOffTime.dateTime === liveFixture.Date &&
+          fixture.homeTeam.countryCode === liveFixture.HomeTeam.IdCountry &&
+          fixture.awayTeam.countryCode === liveFixture.AwayTeam.IdCountry
+      );
+
+      if (matchingFixture) {
+        matchingFixture.status = 'LIVE';
+        matchingFixture.score = {
+          total: {
+            away: liveFixture.AwayTeam.Score,
+            home: liveFixture.HomeTeam.Score,
+          },
+        };
+      }
+    });
   }
 
   @task
@@ -123,6 +172,11 @@ export default class Api extends Service {
   @task
   *loadStandings(): TaskGenerator<Array<GroupStandingWireFormat>> {
     return yield this.fetch(standingsEndpoint);
+  }
+
+  @task
+  *liveScores(): TaskGenerator<any> {
+    return yield this.fetch(liveScoreEndpoint);
   }
 
   async fetch(endpoint: string) {
